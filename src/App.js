1fd24c3ad14b359826deecd8e30671b1dce83038
Anthony Torres <anthony_torres@me.com>
2019-12-09 09:37:14 -0600

    can now create building address

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import LoginRegisterForm from './LoginRegisterForm'
+// import SearchAddBuildingForm from './SearchAddBuildingForm'
 import SearchAddBuildingForm from './SearchAddBuildingForm'
 
 class App extends React.Component{
 	constructor(){
 		super()
 		this.state={
+			addresses: [],
 			loggedIn: false,
-			loggedInUser: null
+			loggedInUser: null,
+			showModalOpen: false,
+			createModalOpen:false,
+			newAddress: {
+				address: ''
+			}
 		}
 	}
 
@@ -21,8 +28,8 @@ class App extends React.Component{
 				'Content-Type': 'application/json'
 			}
 		})
-		console.log(response);
 		const parsedLoginResponse = await response.json()
+		console.log(parsedLoginResponse);
 	
 		if(response.status === 201){
 			this.setState({
@@ -57,20 +64,84 @@ class App extends React.Component{
 		}
 	}
 
+	addBuilding = async (e) => {
+		e.preventDefault();
+		console.log("we're sending this");
+		console.log(this.state.newAddress)
+		try {
+			const createdAddressResponse = await fetch(
+				process.env.REACT_APP_API_URL + "/building/",
+				{
+					method: 'POST',
+					credentials: 'include',
+					body: JSON.stringify(this.state.newAddress),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+			);
+			console.log(createdAddressResponse)
+				const parsedResponse = await createdAddressResponse.json();
+				console.log(parsedResponse, 'this is the response')
+				this.setState({
+					addresses: [
+					...this.state.addresses,
+					parsedResponse.data
+					]
+				})
+				this.closeModal();
+
+			
+		} catch(err){
+			console.log(err)
+		}
+	}
+	handleCreateChange = (event) => {
+		event.preventDefault();
+		console.log(" we are hitting the handleCreateChange function");
+		this.setState({
+			newAddress: {
+				// ...this.state.newAddress,
+				[event.target.name]: event.target.value
+			}
+		})
+	}
+
 	logout = async (loginInfo) => {
 		this.setState({
 			loggedIn: false
 		})
 	}
 
+	showModalOpen = () => {
+		this.setState({
+			showModalOpen: true
+		})
+	}
+
+	closeModal = () => {
+		this.setState({
+			editModalOpen: false,
+			createModalOpen: false,
+			showModalOpen: false
+		});
+	};
+
 	render(){
 		return(
 	    	<div className="App">
 	    		{
-	    			// this.state.loggedInUser
-	    			// ?
-	    			// <SearchAddBuildingForm logout={this.logout} user={this.state.loggedInUser} />
-	    			// :
+	    			this.state.loggedInUser
+	    			?
+	    			<SearchAddBuildingForm 
+
+	    				open={this.state.createModalOpen}
+	    				addBuilding={this.addBuilding}
+	    				handleCreateChange={this.handleCreateChange}
+	    			 	closeModal={this.closeModal} 
+	    			 	address={this.state.newAddress.address}
+	    			/>
+	    			:
 	    			<LoginRegisterForm login={this.login} register={this.register} />
 	    		}
 	    	</div>
--- src/SearchAddBuildingForm/index.js
+++ src/SearchAddBuildingForm/index.js
@@ -1,24 +1,146 @@
-import React from 'react'
+// import React from 'react'
+import React from "react";
+import '../App.css'
+import {
+	Form,
+	Button,
+	Label,
+	Header,
+	Modal,
+	Segment,
+	Image
+} from "semantic-ui-react";
 
 
-// class Building
-// class SearchAddForm extends React.Component{
+class SearchAddBuildingForm extends React.Component {
+	constructor(){
+		super()
+
+		this.state={
+			propertyAddress: '',
+			action: 'search'
+	}
+
+}		
+
+searchAdd = () => {
+	if(this.state.action === 'search') {
+		this.props.search({
+			address: this.state.address
+		})
+	} else{
+		this.props.add({
+			address: this.state.address
+		})
+	}
+}
+
+switchForm = () => {
+	if(this.state.action === 'search'){
+		this.setState({
+			action: 'add'
+		})
+	} else{
+		this.setState({
+			action: 'search'
+		})
+	}
+}
+
+handleChange = (e) => {
+	this.setState({
+		[e.target.name]: e.target.value	
+	})
+}
+
+handleSubmit = (e) => {
+	e.prevernDefault()
+	this.searchAdd()
+}
+
+render(){
+	return (
+
+		<Modal open={true} closeIcon onClose={this.props.closeModal}>
+			
+			{
+				this.state.action === 'search'
+				?
+				<Modal.Content>
+					<Modal.Description>
+						<Header>Search for your building</Header>
+						<Form onSubmit={this.props.addBuilding}>
+							<Label>Address:</Label>
+							<Form.Input
+								type="text"
+								name="address"
+								value={this.props.address}
+								onChange={this.props.handleCreateChange}
+							/>
+							<Modal.Actions>
+								<Button color="yellow" onClick={this.props.closeModal}>
+									Cancel
+								</Button>
+								<Button
+									color='green'
+									type='Submit'
+									content="Create address!"
+								/>
+							</Modal.Actions>
+						</Form>
+					</Modal.Description>
+				</Modal.Content> 
+				:
+				<Modal.Content>
+					<Modal.Description>
+						<Header>Add your current building</Header>
+						<Form
+							onSubmit={this.props.addBuilding}>
+							<Label>Address:</Label>
+							<Form.Input
+								type="text"
+								name="address"
+								value={this.props.address}
+								onChange={this.props.handleCreateChange}
+							/>
+							<Modal.Actions>
+								<Button color="black" onClick={this.props.closeModal}>
+									Cancel
+								</Button>
+								<Button
+									color='green'
+									type='Submit'
+									content="Create address!"
+								/>
+							</Modal.Actions>
+
+						</Form>
+
+		            </Modal.Description>
+				</Modal.Content>
+
+			}
+
+			{
+              this.state.action === "search"
+              ?
+              <small >Dont see your building? Add it <span onClick={this.switchForm}>here</span>.</small>
+              :
+              <small >Search for your building<span onClick={this.switchForm}>here</span>!</small>  
+            }
+
+		</Modal>
+
+		);
+	}
+}
+
+
+export default SearchAddBuildingForm
 
 
 
 
-// 	render(){
-// 		return(
-// 			<div>
-
-// 				hi
-// 				</div>
-// 			)
-// 	}
-// }
 
 
 
-
-
-// export default SearchAddForm
